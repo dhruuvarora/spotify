94244da89fe7875159f2388485e3205212d556c9
Dhruv Arora <dhruv862arora@gmail.com>
2025-01-22 12:14:41 +0530

    Add Song to Playlist and Get playlist of particular Artist

--- spotify_backend/routes/playlist.js
+++ spotify_backend/routes/playlist.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const Playlist = require("../models/Playlist");
+const User = require("../models/User");
+const Song = require("../models/Song");
 
 // create a playlist route
 
@@ -21,7 +23,7 @@ router.post("/create",passport.authenticate("jwt", {session:false}),async (req,r
 
 // router-2 get playlist by id
 //  we are using ':' this means that playlist id is variable and we can assign any value
-router.get("/get/:playlistId" , passport.authenticate("jwt", {session:false}), async (req,res)=>{
+router.get("/get/playlist/:playlistId" , passport.authenticate("jwt", {session:false}), async (req,res)=>{
 
     // params ---> used for calling api by giving input in get 
     const playlistId = req.params.playlistId;
@@ -34,5 +36,64 @@ router.get("/get/:playlistId" , passport.authenticate("jwt", {session:false}), a
     }
     return res.status(200).json(playlist);
 });
+
+
+// get all playlist made by an artist
+router.get("/get/artist/:artistId" , passport.authenticate("jwt" , {session:false}) , async(req,res)=>{
+    const artistId = req.params.artistId;
+
+
+    // error handling : check if artists Id exists
+
+    const artist = await User.findOne({_Id:artistId});
+
+    if(!artist){
+        return res.status(304).json({error:"Invalid Artist Id"});
+    }
+
+    // finding artist based on songs or playlist
+
+    const playlists = await Playlist.find({owner:artistId});
+    
+
+    return res.status(200).json({data:playlists});
+})
+
+
+// add a song to playlist
+
+
+router.post("/add/song" , passport.authenticate("jwt" , {session:false}) , async (req,res)=>{
+    const currentUser = req.user;
+
+    const {playlistId , songId} = req.body;
+
+// Step- 0 Get playlist if Valid
+
+    const playlist = await Playlist.findOne({_id:playlistId});
+    if(!playlist){
+        return res.status(304).json({error:"Playlist Doesn't Exist"});
+    }
+
+    // step 1 : check if current user owns playlist || is a collaborator
+
+    if(playlist.owner != currentUser._id && !playlist.collaborators.includes(currentUser._id)){
+        return res.status(400).json({error:"Not Allowed"})
+    }
+
+    // step-2 Check if song is valid song
+
+    const song = await Song.findOne({_id:songId});
+    if(!song){
+        return res.status(304).json({error:"Song Doesn't exists"});
+    }
+
+    // Step-3 We can Now simply add the song to the playlist
+
+    playlist.songs.push(songId);
+    await playlist.save();
+
+    return res.status(200).json(playlist);
+})
 
 module.exports = router;
